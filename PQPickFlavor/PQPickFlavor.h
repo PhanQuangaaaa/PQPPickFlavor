b57838d38d508512dc9b885ae461155cbe62882f
Phan Quang <phanquang2992@gmail.com>
2022-10-07 15:55:05 +0700

    Initial commit

---
+++ PQPickFlavor/PQPickFlavor.h
@@ -0,0 +1,18 @@
+//
+//  PQPickFlavor.h
+//  PQPickFlavor
+//
+//  Created by Admin on 07/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PQPickFlavor.
+FOUNDATION_EXPORT double PQPickFlavorVersionNumber;
+
+//! Project version string for PQPickFlavor.
+FOUNDATION_EXPORT const unsigned char PQPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PQPickFlavor/PublicHeader.h>
+
+
